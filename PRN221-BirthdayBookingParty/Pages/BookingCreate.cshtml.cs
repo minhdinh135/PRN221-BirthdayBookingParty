18c200e84e309c553b1c98f3b998a95ec6b483a9
Minh Dinh <macotontai123@gmail.com>
2024-03-01 17:17:50 +0700

    update entity with property PartyDateTime with migrations, add Booking Create feature

---
+++ PRN221-BirthdayBookingParty/Pages/BookingCreate.cshtml.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Models;
+using Repositories;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+
+namespace PRN221_BirthdayBookingParty.Pages
+{
+    [BindProperties]
+    public class BookingCreateModel : PageModel
+    {
+        public DateTime BookingDate { get; set; }
+        public DateTime PartyDateTime { get; set; }
+        public int Status { get; set; }
+        public int PackageId { get; set; }
+        public int RoomId { get; set; }
+
+        private BookingRepository bookingRepository;
+
+        public BookingCreateModel()
+        {
+            bookingRepository = new BookingRepository();
+        }
+        public void OnGet()
+        {
+            BookingDate = DateTime.Now;
+        }
+
+        public IActionResult OnPost()
+        {
+            string userString = HttpContext.Session.GetString("CUSTOMER");
+            var user = JsonSerializer.Deserialize<User>(userString);
+            int userId = user.UserId;
+
+            Booking booking = new Booking
+            {
+                BookingDate = DateTime.Now,
+                PartyDateTime = PartyDateTime,
+                BookingStatus = "2",
+                PackageId = PackageId,
+                Feedback = "N/A",
+                RoomId = RoomId,
+                UserId = userId
+            };
+
+            bookingRepository.Add(booking);
+
+            return RedirectToPage("/BookingList");
+        }
+    }
+}
