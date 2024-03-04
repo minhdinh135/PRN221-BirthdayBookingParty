118e3b8445466daa235c4cf0603fd5c80c2e92bb
Minh Dinh <macotontai123@gmail.com>
2024-03-04 22:52:12 +0700

    feat: add Delete Booking for Customer Role

---
+++ PRN221-BirthdayBookingParty/Pages/BookingDelete.cshtml.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Models;
+using Repositories;
+
+namespace PRN221_BirthdayBookingParty.Pages
+{
+    [BindProperties]
+    public class BookingDeleteModel : PageModel
+    {
+        private BookingRepository bookingRepository;
+
+        public int BookingId { get; set; }
+
+        public BookingDeleteModel()
+        {
+            bookingRepository = new BookingRepository();
+        }
+
+        public void OnGet(int id)
+        {
+            BookingId = id;
+        }
+
+        public IActionResult OnPost(int id)
+        {
+            Booking booking = bookingRepository.GetAll().FirstOrDefault(b => b.BookingId == id);
+
+            bookingRepository.Delete(booking);
+
+            return RedirectToPage("/BookingList");
+        }
+    }
+}
