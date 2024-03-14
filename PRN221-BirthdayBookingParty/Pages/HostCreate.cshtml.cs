7f48f342ac3663f456259546a4e25fdcac91bf36
hoanglam1811 <nguyenhoanglam18112003@gmail.com>
2024-03-14 23:00:36 +0700

    add UpdateHost

--- PRN221-BirthdayBookingParty/Pages/HostCreate.cshtml.cs
+++ PRN221-BirthdayBookingParty/Pages/HostCreate.cshtml.cs
@@ -25,7 +25,7 @@ namespace PRN221_BirthdayBookingParty.Pages
         [BindProperty]
         public string RoleId { get; set; }
         [BindProperty]
-        public DateTime Birthday { get; set; }
+        public DateTime Birthday { get;set; } = DateTime.Now;
 
         private IRepositoryBase<User> _userRepository;
         public HostCreateModel()
